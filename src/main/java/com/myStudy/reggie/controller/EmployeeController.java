7977674d3266447b72c3401730365d16a0310135
Hao Huang <areshuang1@gmail.com>
2023-08-15 00:54:37 -0400

    finish create employee function

--- src/main/java/com/myStudy/reggie/controller/EmployeeController.java
+++ src/main/java/com/myStudy/reggie/controller/EmployeeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -62,4 +63,21 @@ public class EmployeeController {
         request.getSession().removeAttribute("employee");
         return R.success("logout success");
     }
+    @PostMapping
+        public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
+        log.info("new employee is {}", employee.toString());
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        Long empId = (Long)request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        return R.success("create employee successfully");
+    }
+
+
 }
